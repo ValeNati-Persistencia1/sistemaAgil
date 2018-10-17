725ad8e7ec7e2bd5edb7befac7c6f3f86aace135
valeria <valeriagrandi@hotmail.com>
2018-10-17 13:48:29 -0300

    borrardo del nombre en SpringBean

--- src/main/java/ar/sarm/unq/sga/wicket/HomePage.java
+++ src/main/java/ar/sarm/unq/sga/wicket/HomePage.java
@@ -10,13 +10,12 @@ import ar.sarm.unq.sga.wicket.backlog.BacklogPage;
 import ar.sarm.unq.sga.wicket.project.ProjectPage;
 import ar.sarm.unq.sga.wicket.userstory.UserStoryPage;
 
-
 public class HomePage extends WebPage {
 	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(HomePage.class);
 	private static final long serialVersionUID = 1L;
 	
-//	@SpringBean(name ="project")
-//	private Home <Project> project; 
+	@SpringBean
+	private Home <Project> project; 
 	public HomePage() {
 		log.debug("construyendo el formulario");
 		this.add(new Link<String>("proyecto") {
