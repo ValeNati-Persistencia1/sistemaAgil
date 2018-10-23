50b2716827e9ce99b5efa5dfdcbd992dcdd7514e
valeria <valeriagrandi@hotmail.com>
2018-10-23 14:17:22 -0300

    agregado de una variable en el generateMain

--- src/test/java/ar/sarm/unq/sga/GenerateDataMain.java
+++ src/test/java/ar/sarm/unq/sga/GenerateDataMain.java
@@ -2,9 +2,12 @@ package ar.sarm.unq.sga;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import ar.sarm.unq.sga.hibernate.HibernateConf;
+
 public class GenerateDataMain {
 
 	public static void main(String[] args) {
+		HibernateConf.modo = "create";
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
 		ctx.scan("ar.sarm.unq.sga" , "ar.sarm.unq.sga.wicket", "ar.sarm.unq.sga.hibernate", "ar.sarm.unq.sga.home","ar.sarm.unq.sga.model");
 		ctx.refresh();
