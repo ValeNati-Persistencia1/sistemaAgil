466ace57fe9148a17eb08acee81d729a64101da1
valeria <valeriagrandi@hotmail.com>
2018-10-23 14:16:19 -0300

    borrado de los atributos no necesarios

--- src/test/java/ar/sarm/unq/sga/GenerateData.java
+++ src/test/java/ar/sarm/unq/sga/GenerateData.java
@@ -1,39 +1,23 @@
 package ar.sarm.unq.sga;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import ar.sarm.unq.sga.home.Home;
 import ar.sarm.unq.sga.model.Project;
 
 @Component
+@Transactional
 public class GenerateData {
-	@Autowired
-	private SessionFactory sessionFactory;
 
 	@Autowired
 	private Home<Project> projectStore;
-	
 
 	protected void generate() {
-		Session s =sessionFactory.getCurrentSession();
-		Transaction transaction = s.beginTransaction();
+		Project proyecto = new Project("proyectito2");
+		projectStore.insert(proyecto);
 
-		try {
-			Project proyecto = new Project("proyectito2");
-			projectStore.insert(proyecto);
-			transaction.commit();
-			
-		} catch (RuntimeException e) {
-			transaction.rollback();
-			throw e;
-			
-		} finally {
-			s.close();
-		}
 	}
 
 }
