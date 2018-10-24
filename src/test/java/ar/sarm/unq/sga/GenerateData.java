110ae87f4b59c1993a5d6ac878bb587809358641
valeria <valeriagrandi@hotmail.com>
2018-10-24 20:22:41 -0300

    iniciando una  transaccion a mano, inyeccion del store

--- src/test/java/ar/sarm/unq/sga/GenerateData.java
+++ src/test/java/ar/sarm/unq/sga/GenerateData.java
@@ -1,42 +1,32 @@
 package ar.sarm.unq.sga;
 
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
-import ar.sarm.unq.sga.home.Home;
-import ar.sarm.unq.sga.model.Developer;
 import ar.sarm.unq.sga.model.Project;
-import ar.sarm.unq.sga.wicket.developer.DeveloperStore;
+import ar.sarm.unq.sga.wicket.project.ProjectStore;
+
 @Component
-@Transactional
 public class GenerateData {
-	
 
 	@Autowired
-	private Home<Project> projectStore;
-	
-	@Autowired Home<Developer>developerStore;
+	private ProjectStore projectStore;
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	// @Autowired Home<Developer>developerStore;
 
 	protected void generate() {
 		Project proyecto = new Project("proyectito2");
+
+		Transaction ts = sessionFactory.getCurrentSession().beginTransaction();
 		projectStore.insert(proyecto);
-		
-		Developer dev=new Developer("Jose");
-		developerStore.insert(dev);
-		
-		
-		
-		
-		
-		
-		
+		// acá van los .insert
+		ts.commit();
+		// Developer dev=new Developer("Jose");
+		// developerStore.insert(dev);
+
 	}
 }
-	
-	
-	
-	
