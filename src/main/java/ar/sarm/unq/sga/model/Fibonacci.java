ffc143a9eb247fda712b17d9761eafb00a48ff8c
nataliaMarzec <natimarzec708@gmail.com>
2018-12-06 20:23:25 -0300

    agregado de constructor en Enum Fibonacci

--- src/main/java/ar/sarm/unq/sga/model/Fibonacci.java
+++ src/main/java/ar/sarm/unq/sga/model/Fibonacci.java
@@ -1,5 +1,19 @@
 package ar.sarm.unq.sga.model;
 
 public enum Fibonacci {
-     UNO, TRES, CINCO, INFINITO;
+	UNO(1.0), DOS(2.0), TRES(3.0), CINCO(5.0), INFINITO(Double.POSITIVE_INFINITY);
+
+	private double numero;
+
+	private Fibonacci(Double numero) {
+		this.setNumero(numero);
+	}
+
+	public double getNumero() {
+		return numero;
+	}
+
+	public void setNumero(double numero) {
+		this.numero = numero;
+	}
 }
